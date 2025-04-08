53a0f1fea71cffcb1cebcaff00261f3d904cdff1
Cybersport <Cybersport@26kadr.internal>
2025-04-08 12:21:21 +0300

    second commit

--- tests/calc_test.py
+++ tests/calc_test.py
@@ -1,3 +1,5 @@
+import pytest
+
 from src.calc import Calculator
 
 calc = Calculator()
@@ -5,6 +7,17 @@ calc = Calculator()
 def test_sum():
     assert calc.sum(4, 6) == 10;
 
-
 def test_multiply():
     assert calc.multiply(12, 3) == 36;
+
+@pytest.mark.parametrize(
+    "a, b, expected",
+    [
+        (3, 5, 8),
+        (-2, 7, 5),
+        (0, 0, 0)
+    ]
+)
+def test_sum(a, b, expected):
+    """Проверка сложения"""
+    assert calc.sum(a, b) == expected
